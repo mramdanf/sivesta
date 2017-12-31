7e3eb9fd2735da3f9f21ee9fa130e9d8bff4dd09
arifwidiyatmiko <arifwidiyatmiko@gmail.com>
2017-12-31 20:03:27 +0700

    update db terbaru

--- server-php/si_vesta.sql
+++ server-php/si_vesta.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 18, 2017 at 03:13 PM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 5.6.31
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2017 at 02:03 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.5.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,6 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_artikel`
+--
+
+CREATE TABLE `tb_artikel` (
+  `id_artikel` varchar(5) NOT NULL,
+  `judul` varchar(200) DEFAULT NULL,
+  `konten` text,
+  `penulis` varchar(200) DEFAULT NULL,
+  `tgl_posting` date DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_artikel`
+--
+
+INSERT INTO `tb_artikel` (`id_artikel`, `judul`, `konten`, `penulis`, `tgl_posting`, `image`) VALUES
+('A0001', 'Peluang di Circular Economy', '<p style="text-align: justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n\n<p style="text-align: justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n\n<p style="text-align: justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 'Arif Widiyatmiko', '2017-12-11', 'Circular_Economy.png'),
+('A0002', 'Cengkeh Indonesia', '<p style="text-align: justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n\r\n<p style="text-align: justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n\r\n<p style="text-align: justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 'Arif Widiyatmiko', '2017-12-11', 'kebangkitan_cengkeh.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_funders`
 --
 
@@ -35,15 +56,16 @@ CREATE TABLE `tb_funders` (
   `telepon` varchar(13) NOT NULL,
   `email` varchar(35) NOT NULL,
   `username` varchar(100) DEFAULT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(50) NOT NULL,
+  `profile_image` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_funders`
 --
 
-INSERT INTO `tb_funders` (`id_funders`, `nama`, `alamat`, `telepon`, `email`, `username`, `password`) VALUES
-('', 'ramdan', 'bogor', '081999111777', 'mramdanf@gmail.com', 'ramdan', '889752dcb81b4ad98ad6e36e9db2cd43');
+INSERT INTO `tb_funders` (`id_funders`, `nama`, `alamat`, `telepon`, `email`, `username`, `password`, `profile_image`) VALUES
+('F00001', 'murdi', '', '', 'murdi@sivesta.com', NULL, '0d55568f98eae7b013df3fe97de2db29', NULL);
 
 -- --------------------------------------------------------
 
@@ -57,19 +79,22 @@ CREATE TABLE `tb_komoditas` (
   `harga` int(11) NOT NULL,
   `stock` int(11) NOT NULL,
   `lokasi` text NOT NULL,
+  `image` varchar(100) DEFAULT NULL,
   `latitude` float DEFAULT NULL,
   `longitude` float DEFAULT NULL,
-  `min_kontrak` int(11) NOT NULL,
-  `profit` int(11) NOT NULL
+  `min_kontrak` int(11) DEFAULT NULL,
+  `profit` float DEFAULT NULL,
+  `deskripsi` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_komoditas`
 --
 
-INSERT INTO `tb_komoditas` (`id_komoditas`, `nama`, `harga`, `stock`, `lokasi`, `latitude`, `longitude`, `min_kontrak`, `profit`) VALUES
-('K00001', 'Forage Crops', 1304000, 500, 'Cibadak', 0, 0, 0, 0),
-('K00002', 'Muntok White Papper', 1650000, 500, 'Cibadak', 0, 0, 0, 0);
+INSERT INTO `tb_komoditas` (`id_komoditas`, `nama`, `harga`, `stock`, `lokasi`, `image`, `latitude`, `longitude`, `min_kontrak`, `profit`, `deskripsi`) VALUES
+('K00001', 'Forage Crops', 1304000, 500, 'Cibadak', 'forage_crops.jpg', 0, 0, 2, 15, '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel blandit elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam eu auctor magna. Aliquam sollicitudin urna quis diam auctor, ac maximus ex dignissim. Nulla facilisi. Fusce a mauris purus. Aliquam tellus nulla, blandit non eleifend eu, efficitur vel arcu. Etiam ultricies lobortis nisl, vitae vehicula libero volutpat a. Morbi congue sapien ut bibendum pharetra. Quisque id pulvinar lectus, ac maximus elit.</p>\n\n<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque felis nisl, malesuada ac aliquam nec, mattis non ligula. Praesent euismod nec urna sed ullamcorper. Morbi consectetur vulputate sapien, eget facilisis sapien semper id. Nulla vitae commodo lorem, non ornare massa. Donec lorem velit, hendrerit imperdiet ex eu, eleifend vehicula nunc. Nulla consequat sapien ac suscipit tincidunt. Proin sed condimentum ligula. Proin posuere, diam ac tempor ultrices, libero augue elementum sapien, ut cursus ligula libero id libero. Donec faucibus magna in auctor bibendum. Morbi cursus pretium augue ac porttitor. Maecenas dictum vitae magna eget interdum. Pellentesque vulputate dignissim facilisis. Proin orci est, lobortis non nunc sed, viverra fringilla nulla. Quisque bibendum sollicitudin lorem, auctor finibus nisl tincidunt non.</p>\n\n<p>In tempus placerat dui, at tempus ante dignissim vel. Vivamus auctor tellus et nunc interdum scelerisque. Integer eros urna, porttitor id leo id, bibendum euismod ligula. Morbi sollicitudin mi ante, rutrum pharetra tortor fringilla et. Nulla laoreet, nulla quis convallis fermentum, mi eros ultricies turpis, sit amet pharetra ligula tortor ac quam. Proin malesuada eget nulla id ullamcorper. Integer porta vulputate felis, non cursus tortor blandit eget. Aliquam sed imperdiet nisi. Quisque pulvinar erat eu tristique maximus. In non facilisis metus, quis ultricies neque. Proin in mi auctor, dictum nulla at, malesuada lacus. Nunc finibus, turpis eleifend egestas bibendum, tortor nibh pharetra nisl, non egestas eros lacus sit amet augue. Maecenas ultricies viverra ligula tristique malesuada. Nam elementum aliquet iaculis. Phasellus eu arcu auctor, aliquet quam non, fringilla neque.</p>'),
+('K00002', 'Muntok White Papper', 1650000, 500, 'Cibadak', 'muntok_white.jpg', 0, 0, 3, 12, '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel blandit elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam eu auctor magna. Aliquam sollicitudin urna quis diam auctor, ac maximus ex dignissim. Nulla facilisi. Fusce a mauris purus. Aliquam tellus nulla, blandit non eleifend eu, efficitur vel arcu. Etiam ultricies lobortis nisl, vitae vehicula libero volutpat a. Morbi congue sapien ut bibendum pharetra. Quisque id pulvinar lectus, ac maximus elit.</p>\n\n<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque felis nisl, malesuada ac aliquam nec, mattis non ligula. Praesent euismod nec urna sed ullamcorper. Morbi consectetur vulputate sapien, eget facilisis sapien semper id. Nulla vitae commodo lorem, non ornare massa. Donec lorem velit, hendrerit imperdiet ex eu, eleifend vehicula nunc. Nulla consequat sapien ac suscipit tincidunt. Proin sed condimentum ligula. Proin posuere, diam ac tempor ultrices, libero augue elementum sapien, ut cursus ligula libero id libero. Donec faucibus magna in auctor bibendum. Morbi cursus pretium augue ac porttitor. Maecenas dictum vitae magna eget interdum. Pellentesque vulputate dignissim facilisis. Proin orci est, lobortis non nunc sed, viverra fringilla nulla. Quisque bibendum sollicitudin lorem, auctor finibus nisl tincidunt non.</p>\n\n<p>In tempus placerat dui, at tempus ante dignissim vel. Vivamus auctor tellus et nunc interdum scelerisque. Integer eros urna, porttitor id leo id, bibendum euismod ligula. Morbi sollicitudin mi ante, rutrum pharetra tortor fringilla et. Nulla laoreet, nulla quis convallis fermentum, mi eros ultricies turpis, sit amet pharetra ligula tortor ac quam. Proin malesuada eget nulla id ullamcorper. Integer porta vulputate felis, non cursus tortor blandit eget. Aliquam sed imperdiet nisi. Quisque pulvinar erat eu tristique maximus. In non facilisis metus, quis ultricies neque. Proin in mi auctor, dictum nulla at, malesuada lacus. Nunc finibus, turpis eleifend egestas bibendum, tortor nibh pharetra nisl, non egestas eros lacus sit amet augue. Maecenas ultricies viverra ligula tristique malesuada. Nam elementum aliquet iaculis. Phasellus eu arcu auctor, aliquet quam non, fringilla neque.</p>'),
+('K00003', 'Sugar Cane', 3913000, 500, 'Cibadak', 'sugar_cane.jpg', 0, 0, 1, 12, '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel blandit elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam eu auctor magna. Aliquam sollicitudin urna quis diam auctor, ac maximus ex dignissim. Nulla facilisi. Fusce a mauris purus. Aliquam tellus nulla, blandit non eleifend eu, efficitur vel arcu. Etiam ultricies lobortis nisl, vitae vehicula libero volutpat a. Morbi congue sapien ut bibendum pharetra. Quisque id pulvinar lectus, ac maximus elit.</p>\n\n<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque felis nisl, malesuada ac aliquam nec, mattis non ligula. Praesent euismod nec urna sed ullamcorper. Morbi consectetur vulputate sapien, eget facilisis sapien semper id. Nulla vitae commodo lorem, non ornare massa. Donec lorem velit, hendrerit imperdiet ex eu, eleifend vehicula nunc. Nulla consequat sapien ac suscipit tincidunt. Proin sed condimentum ligula. Proin posuere, diam ac tempor ultrices, libero augue elementum sapien, ut cursus ligula libero id libero. Donec faucibus magna in auctor bibendum. Morbi cursus pretium augue ac porttitor. Maecenas dictum vitae magna eget interdum. Pellentesque vulputate dignissim facilisis. Proin orci est, lobortis non nunc sed, viverra fringilla nulla. Quisque bibendum sollicitudin lorem, auctor finibus nisl tincidunt non.</p>\n\n<p>In tempus placerat dui, at tempus ante dignissim vel. Vivamus auctor tellus et nunc interdum scelerisque. Integer eros urna, porttitor id leo id, bibendum euismod ligula. Morbi sollicitudin mi ante, rutrum pharetra tortor fringilla et. Nulla laoreet, nulla quis convallis fermentum, mi eros ultricies turpis, sit amet pharetra ligula tortor ac quam. Proin malesuada eget nulla id ullamcorper. Integer porta vulputate felis, non cursus tortor blandit eget. Aliquam sed imperdiet nisi. Quisque pulvinar erat eu tristique maximus. In non facilisis metus, quis ultricies neque. Proin in mi auctor, dictum nulla at, malesuada lacus. Nunc finibus, turpis eleifend egestas bibendum, tortor nibh pharetra nisl, non egestas eros lacus sit amet augue. Maecenas ultricies viverra ligula tristique malesuada. Nam elementum aliquet iaculis. Phasellus eu arcu auctor, aliquet quam non, fringilla neque.</p>');
 
 -- --------------------------------------------------------
 
@@ -88,7 +113,8 @@ CREATE TABLE `tb_komoditas_perenial` (
 
 INSERT INTO `tb_komoditas_perenial` (`id_komoditas`, `jumlah_phon`) VALUES
 ('K00001', 100),
-('K00002', 100);
+('K00002', 100),
+('K00003', 100);
 
 -- --------------------------------------------------------
 
@@ -136,7 +162,8 @@ CREATE TABLE `tb_penanaman` (
 
 INSERT INTO `tb_penanaman` (`id_petani`, `id_komoditas`) VALUES
 ('P001', 'K00001'),
-('P001', 'K00002');
+('P001', 'K00002'),
+('P001', 'K00003');
 
 -- --------------------------------------------------------
 
@@ -157,14 +184,7 @@ CREATE TABLE `tb_petani` (
 --
 
 INSERT INTO `tb_petani` (`id_petani`, `kontak`, `alamat`, `username`, `password`) VALUES
-('P001', '08122233344', 'Ciamis', 'messi', '1463ccd2104eeb36769180b8a0c86bb6'),
-('P002', '432683', 'ssahduashdauuc', NULL, NULL),
-('P003', '432683', 'ssahduashdauuc', NULL, NULL),
-('P004', '432683', 'ssahduashdauuc', NULL, NULL),
-('P005', '432683', 'ssahduashdauuc', NULL, NULL),
-('P006', '432683', 'ssahduashdauuc', NULL, NULL),
-('P007', '92763482', 'sjiosajda', NULL, NULL),
-('P008', '92763482', 'sjiosajda', NULL, NULL);
+('P001', '08122233344', 'Ciamis', 'messi', '1463ccd2104eeb36769180b8a0c86bb6');
 
 -- --------------------------------------------------------
 
@@ -178,13 +198,6 @@ CREATE TABLE `tb_petani_kelompok` (
   `penanggung_jawab` varchar(35) NOT NULL,
   `jumlah_petani` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_petani_kelompok`
---
-
-INSERT INTO `tb_petani_kelompok` (`id_petani`, `nama_kelompok`, `penanggung_jawab`, `jumlah_petani`) VALUES
-('P008', 'Arif Group', 'Prima', 3);
 
 -- --------------------------------------------------------
 
@@ -202,13 +215,17 @@ CREATE TABLE `tb_petani_perorangan` (
 --
 
 INSERT INTO `tb_petani_perorangan` (`id_petani`, `nama`) VALUES
-('P001', 'miftah'),
-('P005', 'arif'),
-('P006', 'arif');
+('P001', 'miftah');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tb_artikel`
+--
+ALTER TABLE `tb_artikel`
+  ADD PRIMARY KEY (`id_artikel`);
 
 --
 -- Indexes for table `tb_funders`
@@ -309,8 +326,8 @@ ALTER TABLE `tb_petani_kelompok`
 --
 ALTER TABLE `tb_petani_perorangan`
   ADD CONSTRAINT `perorangan` FOREIGN KEY (`id_petani`) REFERENCES `tb_petani` (`id_petani`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
