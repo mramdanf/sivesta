2159be5c60958a709bc1d53c843a377b6b90b5b7
arifwidiyatmiko <arifwidiyatmiko@gmail.com>
2017-12-31 20:38:56 +0700

    view artikel name image

--- server-php/application/views/addArtikel.php
+++ server-php/application/views/addArtikel.php
@@ -47,7 +47,7 @@
                   
                 </div>
                 <div class="form-group">
-                  <textarea class="textarea" placeholder="Place some text here"
+                  <textarea class="textarea" name="konten" placeholder="Place some text here"
                           style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
                 </div>
                  <div class="form-group" >
