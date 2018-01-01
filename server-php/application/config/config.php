c86092651bed9aea4f90aeedb24bef8e005a9a43
arifwidiyatmiko <arifwidiyatmiko@gmail.com>
2018-01-01 23:32:13 +0700

    bakup comit

--- server-php/application/config/config.php
+++ server-php/application/config/config.php
@@ -22,8 +22,8 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/sivesta/server-php/';
-$config['base_url'] = 'http://10.99.226.213/sivesta/server-php/';
+$config['base_url'] = 'http://localhost/sivesta/server-php/';
+// $config['base_url'] = 'http://10.99.226.213/sivesta/server-php/';
 
 /*
 |--------------------------------------------------------------------------
