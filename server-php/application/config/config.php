fd39a714b5879845cc3129fbf508977fab08ef38
arifwidiyatmiko <arifwidiyatmiko@gmail.com>
2018-01-07 23:11:49 +0700

    backups

--- server-php/application/config/config.php
+++ server-php/application/config/config.php
@@ -22,9 +22,9 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/sivesta/server-php/';
+$config['base_url'] = 'http://localhost/sivesta/server-php/';
 // $config['base_url'] = 'http://10.99.226.56/sivesta/server-php/';
-$config['base_url'] = 'http://172.20.35.127/sivesta/server-php/';
+// $config['base_url'] = 'http://172.20.35.127/sivesta/server-php/';
 // $config['base_url'] = 'http://192.168.1.127/sivesta/server-php/';
 
 
